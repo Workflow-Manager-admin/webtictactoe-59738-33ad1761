52bac2ee0f566c6f690b0a01a151636a2ebfc443
Kavia AI <production@kavia.ai>
2025-06-25 19:03:21 +0000

    CheckPoint

--- tic_tac_toe_frontend_workspace/tic_tac_toe_frontend/src/routes/Header.svelte
+++ tic_tac_toe_frontend_workspace/tic_tac_toe_frontend/src/routes/Header.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/state';
-	import logo from '$lib/images/svelte-logo.svg';
+	import logo from '$lib/images/svelte-welcome.png';
 
 	// PUBLIC_INTERFACE
 	/**
